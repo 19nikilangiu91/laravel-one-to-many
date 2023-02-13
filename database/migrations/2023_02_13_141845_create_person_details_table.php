e9409191801fab7ce5b610da4a6e454cdedf2c6c
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-13 16:11:38 +0100

    Created Migrations

---
+++ database/migrations/2023_02_13_141845_create_person_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('person_details', function (Blueprint $table) {
+            // Commento "$table->id()" per impostare la colonna "person_id" come chiave primaria
+            // $table->id();
+
+            $table->string('phone', 24)->nullable();
+            $table->string('address');
+            $table->string('avatar')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('person_details');
+    }
+};
