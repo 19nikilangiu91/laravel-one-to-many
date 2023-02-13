240880164bf7a058cec8673da1a1ec9b4bd0ed32
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-13 16:48:55 +0100

    Recalled PersonSeeder in Data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // 7) Richiamo il "PersonSeeder" nel DB
+        $this->call([
+            PersonSeeder::class,
+        ]);
     }
 }
