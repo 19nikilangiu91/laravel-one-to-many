d5d6f76ffce28c148932f35801e1ab55d50587c5
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-13 18:57:21 +0100

    Update home and posts functions

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,12 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+// Importo il Model "Person"
+use App\Models\Person;
+
+// Importo il Model "Posts"
+use App\Models\Post;
+
 class MainController extends Controller
 {
     // Home Route
     public function home()
     {
-        return view('pages.home');
+        $people = Person::all();
+
+        return view('pages.home', compact('people'));
+    }
+
+    // Posts Route
+    public function posts()
+    {
+        $posts = Post::all();
+
+        return view('pages.posts', compact('posts'));
     }
 
 }
