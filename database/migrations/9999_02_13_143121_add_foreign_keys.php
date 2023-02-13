070c7e6c8299a421a6c08c6a7cf8001b27d2c63e
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-13 16:12:40 +0100

    Created foreign_keys migration

---
+++ database/migrations/9999_02_13_143121_add_foreign_keys.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+
+            // 1) Vado a creare una foreignkey alla tabella "posts"
+            $table->foreignId('person_id')
+                ->constrained();
+        });
+
+        Schema::table('person_details', function (Blueprint $table) {
+
+            // 1) Imposto la colonna "person_id" come chiave primaria
+            $table->primary('person_id');
+
+            // 1) Vado a creare una foreignkey alla tabella "person_details"
+            $table->foreignId('person_id')
+                ->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+
+            // 2) dalla struttura della tabella "posts" prendo la chiave "posts_person_id_foreign"
+            // Vado ad eliminare la relazione.
+            $table->dropForeign('posts_person_id_foreign');
+            // Vado ad eliminare la colonna.
+            $table->dropColumn('person_id');
+        });
+
+        Schema::table('person_details', function (Blueprint $table) {
+
+            $table->dropForeign('person_details_person_id_foreign');
+            $table->dropColumn('person_id');
+        });
+    }
+};
